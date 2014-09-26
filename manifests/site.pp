4a8dfc9a17cad790dbde004b75c851af7c613eee
Peter J. Pouliot <peter@pouliot.net>
2014-09-26 09:09:02 -0400

    adding a case to the site.pp to notify you of your environment

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,15 @@
 warning ("You have just installed a R10K git workflow")
+case $environment {
+  'production':{
+    warning ("${fqdn} is in the production environment")
+  }
+  'staging':{
+    warning ("${fqdn} is in the staging environment")
+  }
+  'testing':{
+    warning ("${fqdn} is in the testing environment")
+  }
+  'development':{
+    warning ("${fqdn} is in the development environment")
+  }
+}
