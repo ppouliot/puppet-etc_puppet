5a84b19daf7a78ef1256bb4d9d2398bbaf40a648
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-02 07:45:23 -0400

    updating site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,30 @@
+# == Class: manifests::site
+#
+# This implements Site wide defaults that 
+# need to get run across every host regardless of platform
+#
 notice("Node ${fqdn} is puppet mananaged by the OpenStack Hyper-V CI team")
 
+# Implement Site Wide Time Management
 class {'profiles::time':
   ntpservers => 'pool.ntp.org',
   timezone   => 'UTC',
 } ->
+
+# Remote Access 
 class{'profiles::remote_access':
 } -> 
+
+# Monitoring Agent 
+#class{'profiles::monitoring':
+#} -> 
 
 case $environment {
   'production':{
     warning ("${fqdn} is in the production environment")
+    validate_string($profiles::network_time_configured)
+    validate_string($profiles::remote_access_enabled)
+    validate_string($profiles::monitoring_enabled)
   }
   'staging':{
     warning ("${fqdn} is in the staging environment")
