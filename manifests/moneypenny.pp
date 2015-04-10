13adea00d03109822980afb683f3dbf96a4dfb59
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-10 01:44:30 -0400

    updating moneypenny.pp

--- manifests/moneypenny.pp
+++ manifests/moneypenny.pp
@@ -1,4 +1,8 @@
 node 'moneypenny.openstack.tld'{
+  package{[
+    'expect','screen','minicom','ipmitool','openipmi']:
+    ensure => latest,
+  }
 
   $dhcp_proxy_subnets = '# Additional Networks to listen to
 dhcp-range=10.4.1.0,proxy
@@ -9,15 +13,15 @@ dhcp-range=10.7.1.0,proxy'
 class { 'r10k':
   version           => latest,
   sources           => {
-    'puppetfiles' => {
-      'remote'  => 'https://github.com/ppouliot/puppet-Puppetfile_Env.git',
-      'basedir' => "${::settings::confdir}/environments",
-      'prefix'  => false,
+    'puppetfiles'   => {
+      'remote'      => 'https://github.com/ppouliot/puppet-Puppetfile_Env.git',
+      'basedir'     => "${::settings::confdir}/environments",
+      'prefix'      => false,
     },
-    'hiera' => {
-      'remote'  => 'https://github.com/ppouliot/puppet-hieradata.git',
-      'basedir' => "${::settings::confdir}/hiera",
-      'prefix'  => false,
+    'hiera'         => {
+      'remote'      => 'https://github.com/ppouliot/puppet-hieradata.git',
+      'basedir'     => "${::settings::confdir}/hiera",
+      'prefix'      => false,
     },
   },
   purgedirs         => ["${::settings::confdir}/environments"],
@@ -25,28 +29,59 @@ class { 'r10k':
 }
 
   class {'quartermaster':}
+
+  ## Jenkins Server Settings
   class {'jenkins':
     version            => 'latest',
+    lts                => false,
     install_java       => true,
     repo               => true,
     configure_firewall => true,
     config_hash        => {
       'HTTP_PORT'      => {'value' => '9000' }
+    },
+    ## Jenkins Plugins Added here
+    plugin_hash        => {
+      'parameterized-trigger' => {},
+      'multiple-scms' => {},
+      'git-client' => {},
+      'token-macro' => {},
+      'puppet' => {},
+      'docker-plugin' => {},
+      'docker-build-step' => {},
+      'docker-build-publish' => {},
+      'configure-job-column-plugin' => {},
+      'thinBackup' => {},
+      'backup' => {},
+      'build-node-column' => {},
+      'build-view-column' => {},
+      'built-on-column' => {},
+      'compact-columns' => {},
+      'console-column-plugin' => {},
+      'custom-job-icon' => {},
+      'tfs' => {},
+      'github' => {},
+      'github-api' => {},
+      'git' => {},
+      'git-client' => {},
+      'git-notes' => {},
+      'git-parameter' => {},
+      'git-tag-message' => {},
+      'timestamper' => {}
     }
   }
 
-  jenkins::user { 'jenkins':
+
+  jenkins::user {'jenkins':
     email    => "jenkins@${fqdn}",
     password => 'jenkins',
     public_key => 'ssh-dss AAAAB3NzaC1kc3MAAACBAKyFaUB2hgqiqlj9CMNN6k/SzBTWid07GOHAztQmgreAqdAH8J2Qae+Idq5YQJEZrLi4CCIHe0kPkqFveIZedELYgcLVqIFtVzypLCESySirBp3wM/o7gJgPraKOoJcfygC+WbAEpI12KL8e6DmpZmnLNicWgcFc3xUUC+MsvF8ZAAAAFQCANfgIk7cFFHkacEh3BXksJRGuNQAAAIEAoCOsEzH+0QYMq4Llc7p15pPc59nXIUd/BDUCu87diBqNvQcBPpZXBlmk9tJeEOytjbZs0PKm1izuYFbxISZl4SkcrsA/t3VhHaAXxQSthqfTNiQtqUvE2QDoi+u1LW2P/DnKCAO/oeWjMmNyUapv91ZY1iM6+kYMM7z1EMqs+t4AAACAei66Ns/cARNtYtFZzv1yamdfAInsZIQDvvfeF5PwXwqtFUKBP6o5+jxDQi/OWo/GuTP0wB76Fak2DoyP+jEdtn/m3t2aTmsjiKCvzyjlz3YiZNvCctZuatd9D0NfEj+8iLVovGx5jlz7e+nhxxzjwCggEbgHJh/NbRrFSW8DWbQ= root@norman',
   }
 
-  jenkins::plugin {'puppet':}
-  jenkins::plugin {'docker-plugin':}
-  jenkins::plugin {'docker-build-step':}
-  jenkins::plugin {'docker-build-publish':}
-
 #  class {'jenkins_security':
 #    require => Class['jenkins'],
 #  }
+  class{'jenkins_job_builder':
+    require => Class['jenkins'],
+  }
 }
