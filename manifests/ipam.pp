a3498121ceaa0baa7fe5953a08858d540357c053
primeministerp <primeministerpete@gmail.com>
2016-06-10 14:10:51 -0400

    backup

--- manifests/ipam.pp
+++ manifests/ipam.pp
@@ -1,3 +1,15 @@
-node 'ipam.c2r1.openstack.tld' {
-  notice("!!!!!!!!!!!!!!!!This node is for IPAM testing!!!!!!!!!!!!!!!!!!!!!!")
+# Node Definition for DNS/DHCP infra
+
+node /^(norman|mother|ns[0-9\.]+)/ {
+# Legacy stuff commented out, rest handled by site.pp and hiera.
+#  include basenode::params
+#  package {$nfs_packages:
+#    ensure => latest,
+#  }
+#  create_resources(basenode::nfs_mounts,$nfs_mounts)
+# Sensu is currently disabled
+#  class {'sensu':}
+#  class {'sensu_client_plugins': require => Class['sensu'],}
+  class { 'ipam': }
 }
+
